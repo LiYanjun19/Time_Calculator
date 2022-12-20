cd7cb240bfad57a8db649e8cc26c24cba06dc6e5
Kyle A <kyadamos@gmail.com>
2022-12-19 21:00:03 -0800

    added powershell script

---
+++ Powershell/clockout.ps1
@@ -0,0 +1,13 @@
+# powershell script for clocking out
+
+# read user input for clock in time, lunch start, and lunch end
+
+# create variables for clock-in, lunch start, lunch end
+
+# calculate time worked (time difference between lunch start and clock-in time)
+
+# calculate time left to work (time difference between 8 hours and time worked)
+
+# calculate clock out time (sum of time left to work and lunch end)
+
+# display clock-out time
